5f8d20331989a11de6888130318752ce8307b456
andbalch <andbalch@gmail.com>
2023-11-20 12:34:40 -0500

    TODOs

--- distillation.py
+++ distillation.py
@@ -6,21 +6,30 @@ class ACPolicyDistillation:
         # TODO: Initialize the experiment, including: teacher and student models, 
         # optimization method, distance metric, type of distillation, hyperparameters, etc
         self.args = args
+        # TODO: Setup vectorized env for teacher and student, with the student having a separate task
+        # TODO: Initialize both teacher and student with pre-defined networks for actor and critic
+        # TODO: Setup collectors for teacher and student
+        # TODO: Setup trainer for teacher
+        # TODO: Setup TB logging for teacher and student
+        # TODO: Define objective func/distance metric
 
     def RunVanilla(self):
         """
         Run an experiment with vanilla policy distillation.
-        """        
-        return None
+        """
+        # TODO: Train teacher as per docs until convergence
+        # TODO: Figure out how to train student according to objective function 
+        # (https://tianshou.readthedocs.io/en/master/tutorials/dqn.html#train-a-policy-with-customized-codes)
+        assert NotImplementedError
     
     def RunBootstrap(self):
         """
         Run a policy distillation experiment where the value function is bootstrapped from the Teacher.
         """
-        return None
+        assert NotImplementedError
     
     def RunCriticReward(self):
         """
         Run a policy distillation experiment where the critic is used as intrinsic reward for the Student.
         """
-        return None
+        assert NotImplementedError
